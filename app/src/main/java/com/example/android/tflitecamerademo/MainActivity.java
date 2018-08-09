14897b23032ae67dc8597f114736d8869bc33bf8
bjx <2743801970@qq.com>
2018-08-09 16:21:33 +0800

    AI

---
+++ app/src/main/java/com/example/android/tflitecamerademo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.tflitecamerademo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent=getIntent();
+        System.out.println(intent.getIntExtra("text",1));
+    }
+}
